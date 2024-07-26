2e478c5c76c504bdaf4683742b13d0cbdfa5b4d8
muhammadhadi2006 <muhammadhadiswift@gmail.com>
2024-07-26 15:54:05 +0500

    Add files via upload
    initial commit

---
+++ WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Hadi on 26/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
