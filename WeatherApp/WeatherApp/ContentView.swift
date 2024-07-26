983fd137262ffdae33216dc8f8b90f4506957ffd
Hamza Alam Hashmi <hamzahalam2001@gmail.com>
2024-07-26 16:04:24 +0500

    Working on Weather Degrees Details

--- WeatherApp/WeatherApp/ContentView.swift
+++ WeatherApp/WeatherApp/ContentView.swift
@@ -9,14 +9,24 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ScrollView{
-            VStack {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, Weather App")
+        NavigationStack {
+            ScrollView{
+                VStack {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, Weather App")
+                    
+                    NavigationLink {
+                        WeatherView()
+                    } label: {
+                        Text("Open Weather Deatils")
+                    }
+
+                }
+                .padding()
             }
-            .padding()
+            
         }
     }
 }
---
+++ WeatherApp/WeatherApp/WeatherView.swift
@@ -0,0 +1,21 @@
+//
+//  WeatherView.swift
+//  WeatherApp
+//
+//  Created by Hamza Hashmi on 26/07/2024.
+//
+
+import SwiftUI
+
+struct WeatherView: View {
+    var body: some View {
+        Text("""
+Hello, Weather View!
+Today is 26 degrees
+""")
+    }
+}
+
+#Preview {
+    WeatherView()
+}
